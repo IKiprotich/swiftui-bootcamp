db5d703479ea21eff3710eafdb0216841388d677
Ian Kiprotich <kiprotichian254@gmail.com>
2025-04-21 10:09:40 +0300

    Initial Commit

---
+++ SwiftfulthinkingBootcamp/SwiftfulthinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulthinkingBootcampApp.swift
+//  SwiftfulthinkingBootcamp
+//
+//  Created by Ian   on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulthinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
